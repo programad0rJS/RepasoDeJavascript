38f8b04f49bbe10563942a37614d398946cd775b
josue <83614619+josuefeo123@users.noreply.github.com>
2021-08-30 17:12:52 -0500

    repaso
    repaso

---
+++ 02_RepasoOpcional_Javascript/Main.js
@@ -0,0 +1,40 @@
+// Variables
+
+
+// let Nombre = 'Josue'
+// Nombre  = 'J'
+// console.error('error')
+// console.log(Nombre)
+// var Feo = 'Jos'
+// Feo  ='Pvp '
+
+
+// const Guapo = ' '
+
+// document.write(Nombre) 
+
+// document.write(Feo)
+
+
+// class Nombres {
+//     constructor(){
+//         this.Daniel = '';
+//         this.Xonyu = '';
+//         this.Leo    = '';
+//     }
+//     Nombres(){
+//             this.Leo
+//     }
+//     LosNombre(){
+//         this.Daniel
+//     }
+//     LosNombreFeos(){
+//         this.Xonyu.fontcolor = 'red'
+//     }
+// }
+
+// var Nombre1 = new Nombres('Soy Leo',120,1231 );
+// var Nombre2 = new Nombres('Soy Daniel',120,1231 );
+// var TioRonald  = new Nombres('Soy Xonyu',120,1231 );
+
+// console.log(TioRonald);
---
+++ 02_RepasoOpcional_Javascript/index.md
@@ -0,0 +1,7 @@
+
+https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach;
+
+
+{
+Array.prototype.forEach()
+El método forEach() ejecuta la función indicada una vez por cada elemento del array.};
